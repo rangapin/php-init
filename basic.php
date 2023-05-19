837a7f6f4744c33d1c8b817a46ab1c971aa7b176
rangapin <rangapin@gmx.com>
2023-05-20 00:58:16 +0200

    initial commit

---
+++ basic.php
@@ -0,0 +1,35 @@
+<?php 
+
+$lengh = 10;
+$width = 5;
+
+$rectangleArea = $lengh * $width;
+
+echo "The area of the reactangle is " .  $rectangleArea;
+echo "<br>";
+
+$base = 12;
+$height = 3;
+
+$triangleArea = ($base * $height) / 2;
+
+echo "The area of the triangle is " .  $triangleArea;
+echo "<br>";
+
+$radius = 5;
+
+$circleArea = pow($radius, 2);
+
+echo "The area of the circle is " . $circleArea;
+echo "<br>";
+
+$a = 10;
+$b = 8;
+$height = 4;
+
+$trapeziumArea = (($a + $b) / 2) * 4;
+
+echo "The area of the Trapezium is " . $trapeziumArea;
+echo "<br>";
+
+?>
---
+++ modulus.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstNumber = 100;
+
+$secondNumber = 12;
+
+$result = $firstNumber % $secondNumber;
+
+echo $result;
